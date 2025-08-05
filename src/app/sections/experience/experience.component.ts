fea3b2514504f29cd0af6c6a12bd000d8945f9e3
Leonardo Marotta <sen.aka@hotmail.com>
2025-08-05 12:50:21 -0300

    meio que zero bala mas feio

--- src/app/sections/experience/experience.component.ts
+++ src/app/sections/experience/experience.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatCardModule } from '@angular/material/card'; // <-- IMPORTAR AQUI
+
+// Módulos que este componente usa: Card e agora Icon
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon'; // <-- 1. IMPORTAR AQUI
 
 @Component({
   selector: 'app-experience',
   standalone: true,
-  imports: [CommonModule, MatCardModule], // <-- ADICIONAR AQUI
+  // Adicionar o MatIconModule na lista de imports
+  imports: [CommonModule, MatCardModule, MatIconModule], // <-- 2. ADICIONAR AQUI
   templateUrl: './experience.component.html',
   styleUrls: ['./experience.component.css']
 })
